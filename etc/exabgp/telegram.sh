4d112a8a72889ab1f447eeede99094cd96bbeb0f
ROBSON MENDES <61670676+RobsonMendesRBSTI@users.noreply.github.com>
2022-09-30 00:11:02 -0300

    Update telegram.sh

--- etc/exabgp/telegram.sh
+++ etc/exabgp/telegram.sh
@@ -1,13 +1,10 @@
 ## nano /opt/andrisoft/bin/telegram.sh
-## curl -s --max-time 10 -d "chat_id=-1001533138922&disable_web_page_preview=1&text=WANGUARD: Ataque detectado contra o prefixo TESTE" https://api.telegram.org/bot747889846:AAEbNzmHVU7pEl1Gp3Y3j0VvoI7g4Sot7Kc/sendMessage > /dev/null
+## curl -s --max-time 10 -d "chat_id=-1001826050802&disable_web_page_preview=1&text=WANGUARD: Ataque detectado contra o prefixo TESTE" https://api.telegram.org/bot747889846:AAEbNzmHVU7pEl1Gp3Y3j0VvoI7g4Sot7Kc/sendMessage > /dev/null
 
-#!/usr/bin/env bash
-
-
-
+!/usr/bin/env bash
 #/opt/andrisoft/bin/telegram.sh {prefix} {decoder} {anomaly_id} {sensor} {value_prefix} {unit}
 
-USERID="-1001533138922"
+USERID="-1001826050802"
 KEY="747889846:AAEbNzmHVU7pEl1Gp3Y3j0VvoI7g4Sot7Kc"
 TIMEOUT="10"
 URL="https://api.telegram.org/bot$KEY/sendMessage"
@@ -17,7 +14,6 @@ ANOMALYID=$3
 SENSOR=$4
 ATACKPEAK=$5
 UNITATACKPEAK=$6
-
 
 # EXEMPLOD DE MENSAGEM ENVIADA
 #O Wanguard detectou anomalias de ataque com os seguintes detalhes:
@@ -29,11 +25,11 @@ UNITATACKPEAK=$6
 
 #------
 #foi usado o site urlencoder.io
-#caso necessário, use este site https://www.urlencoder.org/ para escrever o código mais rápido.
-
 
 cat | curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=Sensor%3A%20$SENSOR%0AInterface%3A%20$INTERFACE%0AIP%3A%20$PREFIX%0AVetor%3A%20$VECTOR%0AAnomaliaID%3A%20$ANOMALYID%0APico%3A%20$UNITATACKPEAK" https://api.telegram.org/bot$KEY/sendMessage
 
+#$SENSOR$INTERFACE$PREFIX$VECTOR$
+
 sleep 3
 
-exit
+exit 0
