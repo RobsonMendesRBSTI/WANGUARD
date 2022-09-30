fa73d41f59eb5b216f77e997d0d40710e948d9b7
ROBSON MENDES <61670676+RobsonMendesRBSTI@users.noreply.github.com>
2022-09-30 14:26:21 -0300

    Create EXAMPLE telegram.sh

---
+++ EXAMPLE telegram.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#/opt/andrisoft/bin/telegram.sh {prefix} {decoder} {anomaly_id} {sensor} {value_prefix} {unit}
+
+USERID="-1001510998898"
+KEY="747889846:AAEbNzmHVU7pEl1Gp3Y3j0VvoI7g4Sot7Kc"
+TIMEOUT="10"
+URL="https://api.telegram.org/bot$KEY/sendMessage"
+PREFIX=$1
+VECTOR=$2
+ANOMALYID=$3
+SENSOR=$4
+ATACKPEAK=$5
+UNITATACKPEAK=$6
+
+# EXEMPLOD DE MENSAGEM ENVIADA
+#O Wanguard detectou anomalias de ataque com os seguintes detalhes:
+#Sensor: SENSOR
+#Destino: PREFIX
+#Vetor: VECTOR
+#Pico do ataque: ATACKPEAK UNIT
+#ID da Anomalia: ANOMALYID
+
+#------
+#foi usado o site urlencoder.io
+
+cat | curl -s --max-time $TIMEOUT -d "chat_id=$USERID&disable_web_page_preview=1&text=Sensor%3A%20$SENSOR%0AInterface%3A%20$INTERFACE%0AIP%3A%20$PREFIX%0AVetor%3A%20$VECTOR%0AAnomaliaID%3A%20$ANOMALYID%0APico%3A%20$UNITATACKPEAK" https://api.telegram.org/bot$KEY/sendMessage
+
+#$SENSOR$INTERFACE$PREFIX$VECTOR$
+
+sleep 3
+
+exit 0
